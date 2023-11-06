44025ea90b6440eb7d55333cf1c0d2943ae64cbb
Maury2001 <mmakira01@gmail.com>
2023-11-06 17:19:55 +0300

    rr

---
+++ hash/hashmap.py
@@ -0,0 +1,19 @@
+cities={
+    'kis': 'kisumu',
+    'nai': 'nairobi',
+    'msa': 'mombasa',
+    'nav': 'naivasha'
+}
+
+capital_city = cities['nai']
+
+
+
+hash={
+    'a': 1,
+    'c':2,
+    'b': 1
+}
+even = hash['a']
+
+print('%s ,%d' % (capital_city, even))
--- string_1.py
+++ string_1.py
@@ -4,9 +4,9 @@ script, name = argv
 print("welcome %s" % name)
 
 string_1="bull"
-string_2="shivic"
+string_2="lorem ipsum bitch"
 
 
 print("this is fuckin %s %s" % (string_1, string_2))
 #reverse string
-print (string_2 [::1])
+print (string_2 [::-1])
